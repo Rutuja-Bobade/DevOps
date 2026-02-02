daacef22a0d5e9641b8616ca266f111f888bccad
Rutuja-Bobade <rutuja.bobade23@pccoepune.org>
2026-02-02 12:03:51 +0530

    Java code added

---
+++ Integrate_Jenkins.java
@@ -0,0 +1,6 @@
+public class Integrate_Jenkins {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins !");
+        System.out.println("I am integrating my github repository with jenkins");
+    }
+}
