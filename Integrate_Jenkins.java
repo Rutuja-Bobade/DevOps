2097488f62413d75b6b586bafa6150fe8cb68d1d
Rutuja Bobade <155938957+Rutuja-Bobade@users.noreply.github.com>
2026-02-19 17:17:59 +0530

    Add automatic trigger test message in Jenkins integration

--- Integrate_Jenkins.java
+++ Integrate_Jenkins.java
@@ -3,5 +3,6 @@ public class Integrate_Jenkins {
         System.out.println("Hello, Jenkins !");
         System.out.println("I am integrating my github repository with jenkins");
         System.out.println("Testing Triggers");
+        System.out.println("Testing automatic Triggers");
     }
 }
